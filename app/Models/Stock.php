98a75643ddf1eb45b00645bf2b43699f4f5033f1
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-09-07 11:07:08 +0600

    Stock module functionality implemented

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\PurchaseItem;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -10,4 +11,9 @@ class Stock extends Model
     use SoftDeletes;
 
     protected $guarded = ['id'];
+
+    public function purchaseItems()
+{
+    return $this->hasMany(PurchaseItem::class, 'stock_id');
+}
 }
--- database/migrations/2025_09_07_041135_create_stocks_table.php
+++ database/migrations/2025_09_07_041135_create_stocks_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,16 +13,11 @@ return new class extends Migration
         Schema::create('stocks', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('supplier_id');
-            $table->unsignedBigInteger('stock_item_id');
-            $table->decimal('item_qty')->default(0.00);
-            $table->string('item_unit')->nullable();
-            $table->decimal('item_purchase_price')->default(0.00);
-            $table->decimal('item_total_price')->default(0.00);
-            $table->decimal('total_price')->default(0.00);
             $table->date('stock_entry_date');
             $table->enum('stock_type', ['stock_in', 'stock_out', 'adjustment'])->nullable();
             $table->text('stock_note')->nullable();
             $table->string('invoice')->nullable();
+            $table->decimal('total_price', 12, 2)->default(0.0);
             $table->timestamps();
             $table->softDeletes();
         });
