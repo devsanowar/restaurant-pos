aa93f7a352bf790ddc7fdcd4c1be72c8ea65c1c5
siallinfo <siallinformation@gmail.com>
2025-09-15 10:20:37 +0600

    Update

--- app/Http/Controllers/Admin/StockController.php
+++ app/Http/Controllers/Admin/StockController.php
@@ -14,7 +14,7 @@ class StockController extends Controller
 {
     public function index()
     {
-        $stocks = Stock::git;
+        $stocks = Stock::orderBy('id', 'desc')->paginate(10);
         return view('admin.layouts.pages.stock.index', compact('stocks'));
     }
 }
